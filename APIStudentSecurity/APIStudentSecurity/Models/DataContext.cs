e26d7217fafc322e59fc497024c2f96adc1b777f
lizeth jimenez <a2019117291@estudiantes.upsa.edu.bo>
2020-08-31 18:17:25 -0700

    Proyecto base

---
+++ APIStudentSecurity/APIStudentSecurity/Models/DataContext.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace APIStudentSecurity.Models
+{
+    using System.Data.Entity;
+    public class DataContext: DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+         
+        }
+    }
+}
