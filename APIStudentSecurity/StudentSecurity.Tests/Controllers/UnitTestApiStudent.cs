56e60fe718418dd666fed3476fad51060df6657d
lizeth jimenez <a2019117291@estudiantes.upsa.edu.bo>
2020-09-05 10:17:51 -0700

    Se ha creado el unit test del API Web students

---
+++ APIStudentSecurity/StudentSecurity.Tests/Controllers/UnitTestApiStudent.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using APIStudentSecurity.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StudentSecurity.Tests.Controllers
+{
+    [TestClass]
+    public class UnitTestApiStudent
+    {
+        [TestMethod]
+        public void TestGetStudent()
+        {
+            //Arrange
+            StudentsController studentsController = new StudentsController();
+            //Act
+            var listaEstudiantes = studentsController.GetStudents(); // con s porque es una lista
+            //Assert
+            Assert.IsNotNull(listaEstudiantes);
+        }
+    }
+}
